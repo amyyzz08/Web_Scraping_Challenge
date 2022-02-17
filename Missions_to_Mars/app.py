e3b43055f70110a9286b7ee7488b2a4adfdaff0c
amyzhang <amyyzz08@gmail.com>
2022-02-17 15:14:16 +0800

    in progress

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,19 +1,14 @@
 # Dependencies
-from flask import Flask, render_template
-# from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect 
+from flask_pymongo import PyMongo
 import scrape_mars
 import pymongo
 
 # Creating app
 app= Flask(__name__)
 
-# Connecting to database
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-db = client.mars_db
-
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
-# mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
 
 # Creating route for homepage
 @app.route("/")
@@ -27,6 +22,7 @@ def do_scrape():
     mars_info = mongo.db.mars_data
     mars_data = scrape_mars.scrape_all()
     mars_info.update_one({}, {"$set": mars_data}, upsert=True)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,28 +1,22 @@
 # Dependencies
 from splinter import Browser
-from bs4 import Beautifulsoup as bs
+from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import pymongo
 
 def scrape_all():
 
-    # Connecting to database
-    conn = "mongodb://localhost:27017"
-    client = pymongo.MongoClient(conn)
-    db = client.mars_db
-
     # Setting up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser("chrome", **executable_path, headless=False)
 
-   
-   
-    ## Scraping news title and paragrapgh ##
+
+    # Scraping news title and paragrapgh ##
     url_news = "https://redplanetscience.com/"
     browser.visit(url_news)
 
     html = browser.html
-    soup = bs(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
     news_title_1 = soup.find_all('div', class_='content_title')
     news_p_1 = soup.find_all('div', class_='article_teaser_body')
@@ -36,11 +30,14 @@ def scrape_all():
     url_image = "https://spaceimages-mars.com/"
     browser.visit(url_image)
 
+    full_image_button = browser.find_by_tag('button')[1]
+    full_image_button.click()
+
     html = browser.html
-    soup = bs(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
     image_url = soup.find('img', class_='fancybox-image').get('src')
-    featured_image_url=f"https://spaceimages-mars.com/{image_url}"
+    featured_image_url= url_image + image_url
 
     
     
@@ -51,48 +48,41 @@ def scrape_all():
     browser.visit(url_facts)
 
     table_df = pd.read_html(url_facts)[0]
-    table_df = table_df.columns=["Description","Mars","Earth"]
-    facts_df = table_df.set_index("Description", inplace=True)  
+    table_df.columns=["Description","Mars","Earth"]
+    table_df.set_index("Description", inplace=True)  
 
     
     
     ## Scraping Hemispheres ##
-    hemispheres_image_url=[]
 
     url_hemi = "https://marshemispheres.com/"
     browser.visit(url_hemi)
+  
 
-    html = browser.html
-    soup = bs(html, "html.parser")
+    for x in range(4):
 
+        # browse through each article
+        browser.find_by_css('a.product-item')[x].click()
+        browser.find_by_tag('a.target href')[x].click()
 
-    titles = soup.find_all('h3')
-    
-    for title in titles: 
-        browser.links.find_by_partial_text("Hemisphere")
+        # parse html
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
 
-    hemispheres = soup.find_all("div", class_="item")
-    hemi_image = []
-    hemi_url = []
+        # scrapping
+        hemi_image = soup.find('img').get('src')
+        print(hemi_image)
 
-    for hemisphere in hemispheres:
-        link = hemisphere.find("a")
-        href = link["href"]
-        title = link.text.strip()
+        browser.back()
 
-        image = hemisphere.find("img", class_="thumb")
-        hemi_image.append(url_hemi + image["src"])
-
-    hemi_url.append({"title": title, "image url": hemi_image})
-
-    
+  
     ## Dictionary of information ## 
     mars_data={
         'news_title': title,
         'news_paragraph': paragraph,
-        'featured image': featured_image_url,
-        'facts': facts_df.to_html(classes = "table table-striped"),
-        'hemispheres': hemi_url
+        'featured_image': featured_image_url,
+        'facts': table_df.to_html(classes = "table table-striped"),
+        'hemispheres': hemi_image
     }
 
 
